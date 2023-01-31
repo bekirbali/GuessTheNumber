eeed2e0ea979375dd023232538a382c17d69b1e5
bekirbali <bfbali43@gmail.com>
2023-02-01 00:22:54 +0300

    some bugs fixed

--- number.js
+++ number.js
@@ -14,7 +14,8 @@ let max = 100;
 let min = 1;
 let counter = 0;
 let life = 5;
-button.addEventListener("click", () => {
+button.addEventListener("click", (e) => {
+  e.preventDefault();
   let value = parseInt(inp.value);
   if (inp.value === "") {
     alert("Add a Number");
@@ -25,7 +26,7 @@ button.addEventListener("click", () => {
       alert(`You guessed right, number was ${value}`);
       setTimeout(() => {
         window.location.reload();
-      }, 2000);
+      }, 1000);
     } else if (life === 1) {
       alert(`You run out of life, number was ${random}`);
       window.location.reload();
@@ -52,43 +53,43 @@ button.addEventListener("click", () => {
   lifeCount.innerHTML = `Remaining Life: ${life}`;
 });
 
-inp.addEventListener("keydown", (e) => {
-  console.log(e);
-  if (e.key === "Enter") {
-    let value = parseInt(inp.value);
-    if (inp.value === "") {
-      alert("Add a Number");
-    } else {
-      if (value > 100 || value < 0) {
-        alert("Enter a number between 1-100");
-      } else if (value === random) {
-        alert(`You guessed right, number was ${value}`);
-        setTimeout(() => {
-          window.location.reload();
-        }, 2000);
-      } else if (life === 1) {
-        alert(`You run out of life, number was ${random}`);
-        window.location.reload();
-      } else if (value < random) {
-        console.log("Enter a higher number");
-        min = value;
-        guess.innerHTML = `Number is between -> ${min} and ${max}`;
-        counter += 1;
-        life -= 1;
-        span[0].innerHTML = ` ${counter}`;
-      } else if (value > random) {
-        console.log("Enter a lower number");
-        max = value;
-        guess.innerHTML = `Number is between -> ${min} and ${max}`;
-        counter += 1;
-        life -= 1;
-        span[0].innerHTML = counter;
-      } else {
-        alert("Input has to be a number");
-      }
-    }
-    inp.value = "";
-    inp.focus();
-    lifeCount.innerHTML = `Remaining Life: ${life}`;
-  }
-});
+// inp.addEventListener("keydown", (e) => {
+//   console.log(e);
+//   if (e.key === "Enter") {
+//     let value = parseInt(inp.value);
+//     if (inp.value === "") {
+//       alert("Add a Number");
+//     } else {
+//       if (value > 100 || value < 0) {
+//         alert("Enter a number between 1-100");
+//       } else if (value === random) {
+//         alert(`You guessed right, number was ${value}`);
+//         setTimeout(() => {
+//           window.location.reload();
+//         }, 2000);
+//       } else if (life === 1) {
+//         alert(`You run out of life, number was ${random}`);
+//         window.location.reload();
+//       } else if (value < random) {
+//         console.log("Enter a higher number");
+//         min = value;
+//         guess.innerHTML = `Number is between -> ${min} and ${max}`;
+//         counter += 1;
+//         life -= 1;
+//         span[0].innerHTML = ` ${counter}`;
+//       } else if (value > random) {
+//         console.log("Enter a lower number");
+//         max = value;
+//         guess.innerHTML = `Number is between -> ${min} and ${max}`;
+//         counter += 1;
+//         life -= 1;
+//         span[0].innerHTML = counter;
+//       } else {
+//         alert("Input has to be a number");
+//       }
+//     }
+//     inp.value = "";
+//     inp.focus();
+//     lifeCount.innerHTML = `Remaining Life: ${life}`;
+//   }
+// });
